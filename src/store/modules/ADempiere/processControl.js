4aa888a8db66f9be2e7a84584c61832390c090e4
Yamel Senih <ysenih@erpya.com>
2019-05-09 10:02:39 -0400

    Revert "minimal changes"

--- src/store/modules/ADempiere/processControl.js
+++ src/store/modules/ADempiere/processControl.js
@@ -10,46 +10,33 @@ const processControl = {
     },
     dataResetCacheProcess(state, payload) {
       state.process = payload
-    },
-    deleteProcess(state, payload) {
-      state.process = payload
     }
   },
   actions: {
     // Supported Actions for it
-    startProcess({ commit }, objectParams) {
+    startProcess({ commit }, payload) {
       var processToRun = {
-        uuid: objectParams.containerUuid
+        uuid: payload.action.uuid
       }
-      commit('addStartedProcess', objectParams.containerUuid)
+      console.log(payload)
+      commit('addStartedProcess', processToRun)
       // Run process on server and wait for it for notify
       runProcess(processToRun)
-
-      console.log('TODO: Run here a server process: ' + objectParams.containerUuid)
-    },
-    deleteProcess({ commit, state }, uuid) {
-      var processList = state.process.filter((item) => {
-        if (item !== uuid) {
-          return true
-        }
-      })
-      commit('deleteProcess', processList)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      console.log('TODO: Run here a server process: ' + payload.containerUuid)
     }
   },
   getters: {
-    getRunningProcess: (state, rootGetters, rootState) => {
-      var processList = []
-
-      state.process.map((item) => {
-        var itemProcess = rootGetters.getProcess(item)
-        if (typeof itemProcess !== undefined) {
-          processList.push(itemProcess)
-        }
-      })
-      return processList
-      // return process = state.process.find(
-      //   item => item.uuid === processUuid
-      // )
+    getRunningProcess: (state) => (processUuid) => {
+      var process = state.process.find(
+        item => item.uuid === processUuid
+      )
+      return process
     }
   }
 }
