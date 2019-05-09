c002f8ad519289c753c521cc4b249a384bdc2061
Elsio S <elsiosanches@gmail.com>
2019-05-08 20:09:05 -0400

    solving errors and adding the notification window

--- src/components/ADempiere/ContextMenu/index.vue
+++ src/components/ADempiere/ContextMenu/index.vue
@@ -121,6 +121,12 @@ export default {
     },
     runAction(action) {
       if (action.type === 'action') {
+        this.$message({
+          message: 'Processing',
+          type: 'info',
+          showClose: true
+        })
+        this.$store.dispatch('addProcessExecution', this.$route.meta.uuid)
         this.$store.dispatch(action.action, {
           action: action,
           containerUuid: this.$route.meta.uuid
