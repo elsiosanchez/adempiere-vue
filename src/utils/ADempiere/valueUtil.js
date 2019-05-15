6690cb07afd71bfe0a7fddaeb86976f5b41cd278
Raul Munoz <rmunoz@erpcya.com>
2019-05-15 08:47:30 -0430

    Add method parseContext

--- src/utils/ADempiere/valueUtil.js
+++ src/utils/ADempiere/valueUtil.js
@@ -11,3 +11,50 @@ export function decodeHtml(text) {
   processMetadata.innerHTML = text
   return processMetadata.childNodes[0].nodeValue
 }
+
+// Parse Context String
+export function parseContext(context, value) {
+  if (value === null || value.length() === 0) { return '' }
+
+  var instances = value.length() - value.replace('@', '').length()
+  if ((instances > 0) && (instances % 2) !== 0) { // could be an email address
+    return value
+  }
+
+  var token
+  var inStr = String(value)
+  var outStr = ''
+
+  var i = inStr.indexOf('@')
+  while (i !== -1) {
+    outStr.append(inStr.substring(0, i)) // up to @
+    inStr = inStr.substring(i + 1, inStr.length())	// from first @
+
+    var j = inStr.indexOf('@') // next @
+    if (j < 0) {
+      console.log('No second tag: ' + inStr)
+      return ''	//	no second tag
+    }
+
+    token = inStr.substring(0, j)
+
+    context['columnName'] = token
+
+    var ctxInfo = this.$store.getters.getContext(context)	// get context
+    if (ctxInfo.length() === 0 && (token.startsWith('#') || token.startsWith('$'))) {
+      context['parentUuid'] = null
+      context['containerUuid'] = null
+      ctxInfo = this.$store.getters.getContext(context)	// get global context
+    }
+    if (ctxInfo.length() === 0) {
+      console.log('No Context for: ' + token)
+    } else { outStr = outStr + '' + ctxInfo } // replace context with Context
+
+    inStr = inStr.substring(j + 1, inStr.length())	// from second @
+    i = inStr.indexOf('@')
+  }
+  outStr = outStr + '' + inStr	// add the rest of the string
+
+  return outStr
+}	//	parseContext
+
