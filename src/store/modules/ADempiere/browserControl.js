de45915db4642285f791cc0ee9b3c95135228e31
Edwin Betancourt <EdwinBetanc0urt@hotmail.com>
2019-05-27 19:12:39 -0400

    Add record in browser control

--- src/store/modules/ADempiere/browserControl.js
+++ src/store/modules/ADempiere/browserControl.js
@@ -1,5 +1,5 @@
 import { getBrowserSearch } from '@/api/ADempiere/data'
-import { parseContext } from '@/utils/ADempiere'
+import { convertValueFromGRPC, parseContext } from '@/utils/ADempiere'
 
 const browserControl = {
   state: {
@@ -7,58 +7,76 @@ const browserControl = {
   },
   mutations: {
     addBrowserSearch(state, payload) {
-      state.browserSearch.push(payload)
+      state.browserSearch = payload
     }
   },
   actions: {
     getBrowserSearch({ commit, rootGetters }, browserUuid) {
-      var fieldList = rootGetters.getPanelParameters(browserUuid, true)
-      // console.log(fieldList)
-      if (fieldList.length > 0) {
-        var parameters = fieldList.map(fieldItem => {
-          return {
-            columnName: fieldItem.columnName,
-            value: fieldItem.value
+      return new Promise((resolve, reject) => {
+        var fieldList = rootGetters.getPanelParameters(browserUuid, true)
+        // console.log(fieldList)
+        if (fieldList.length > 0) {
+          var parameters = fieldList.map(fieldItem => {
+            return {
+              columnName: fieldItem.columnName,
+              value: fieldItem.value
+            }
+          })
+
+          var browser = rootGetters.getBrowser(browserUuid)
+          var parsedQuery = parseContext({
+            parentUuid: browserUuid,
+            containerUuid: browserUuid,
+            value: browser.query
+          })
+          var parsedWhereClause = parseContext({
+            parentUuid: browserUuid,
+            containerUuid: browserUuid,
+            value: browser.whereClause
+          })
+
+          var browserSearchQueryParameters = {
+            uuid: browserUuid,
+            query: parsedQuery,
+            whereClause: parsedWhereClause,
+            orderByClause: browser.orderByClause,
+            parameters: parameters
           }
-        })
+          console.log(browserSearchQueryParameters)
+          // Add validation compare browserSearchQueryParameters
+          getBrowserSearch(browserSearchQueryParameters)
+            .then(response => {
+              // commit('addBrowserSearch', browserSearchQueryParameters)
+              var recordList = response.getRecordsList().map((recordItem) => {
+                var values = []
+                recordItem.getValuesMap().forEach((value, key) => {
+                  values.push({ key: key, value: convertValueFromGRPC(value) })
+                })
 
-        var browser = rootGetters.getBrowser(browserUuid)
-        var parsedQuery = parseContext({
-          parentUuid: browserUuid,
-          containerUuid: browserUuid,
-          value: browser.query
-        })
-        var parsedWhereClause = parseContext({
-          parentUuid: browserUuid,
-          containerUuid: browserUuid,
-          value: browser.whereClause
-        })
-
-        var browserSearchQueryParameters = {
-          uuid: browserUuid,
-          query: parsedQuery,
-          whereClause: parsedWhereClause,
-          orderByClause: browser.orderByClause,
-          parameters: parameters
+                return {
+                  id: recordItem.getId(),
+                  uuid: recordItem.getUuid(),
+                  tableName: recordItem.getTablename(),
+                  valuesMap: values
+                }
+              })
+              commit('addBrowserSearch', recordList)
+              resolve(recordList)
+            })
+            .catch(err => {
+              console.warn(err)
+              reject(err)
+            })
+        } else {
+          reject({ error: 0, message: 'No parameters' })
         }
-        console.log(browserSearchQueryParameters)
-        // Add validation compare browserSearchQueryParameters
-        // return new Promise((resolve, reject) => {
-        getBrowserSearch(browserSearchQueryParameters)
-          .then(response => {
-            commit('addBrowserSearch', browserSearchQueryParameters)
-            console.log(response)
-            // resolve(response)
-          })
-          .catch(err => {
-            console.warn(err)
-            // reject(err)
-          })
-        // })
-      }
+      })
     }
   },
   getters: {
+    getResponseBrowser: (state) => () => {
+      return state.browserSearch
+    }
   }
 }
 
