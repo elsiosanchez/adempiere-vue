622e140d1d107840d98ed05901f22ed2f2b1d7f9
Edwin Betancourt <EdwinBetanc0urt@hotmail.com>
2019-06-14 15:19:15 -0400

    Add link to recent items

--- src/views/profile/components/recentItems.vue
+++ src/views/profile/components/recentItems.vue
@@ -3,27 +3,29 @@
     <el-timeline>
       <el-timeline-item v-for="(item, index) of recentItems" :key="index" placement="top">
         <el-card>
-          <h4>{{ item.displayName }}</h4>
-          <ul>
-            <li>
-              Menu Name: {{ item.menuName }}
-            </li>
-            <li>
-              Menu UUID: {{ item.menuUuid }}
-            </li>
-            <li>
-              Window UUID: {{ item.windowUuid }}
-            </li>
-            <li>
-              Table ID: {{ item.tableId }}
-            </li>
-            <li>
-              Record ID: {{ item.recordId }}
-            </li>
-            <li>
-              Tab UUID: {{ item.tabUuid }}
-            </li>
-          </ul>
+          <router-link :to="{ name: item.menuUuid }">
+            <h4>{{ item.displayName }}</h4>
+            <!-- <ul>
+              <li>
+                Menu Name: {{ item.menuName }}
+              </li>
+              <li>
+                Menu UUID: {{ item.menuUuid }}
+              </li>
+              <li>
+                Window UUID: {{ item.windowUuid }}
+              </li>
+              <li>
+                Table ID: {{ item.tableId }}
+              </li>
+              <li>
+                Record ID: {{ item.recordId }}
+              </li>
+              <li>
+                Tab UUID: {{ item.tabUuid }}
+              </li>
+            </ul> -->
+          </router-link>
         </el-card>
       </el-timeline-item>
     </el-timeline>
@@ -38,17 +40,27 @@ export default {
       recentItems: []
     }
   },
+  computed: {
+    getterRecentItems() {
+      return this.$store.getters.getRecentItems
+    }
+  },
   mounted() {
     this.getRecentItems()
   },
   methods: {
     getRecentItems() {
-      this.$store.dispatch('getRecentItemsFromServer')
-        .then(response => {
-          this.recentItems = response
-        }).catch(error => {
-          console.log(error)
-        })
+      var items = this.getterRecentItems
+      if (typeof items === 'undefined' || items.length < 1) {
+        this.$store.dispatch('getRecentItemsFromServer')
+          .then(response => {
+            this.recentItems = response
+          }).catch(error => {
+            console.log(error)
+          })
+      } else {
+        this.recentItems = items
+      }
     }
   }
 }
