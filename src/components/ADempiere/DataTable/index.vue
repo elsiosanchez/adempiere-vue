36ddcf250db05a6f9f99ef44a26f51fcde9158b3
Elsio S <elsiosanches@gmail.com>
2019-07-01 17:27:44 -0400

    height of the table
    * height of the table with the registration counter and selectable

--- src/components/ADempiere/DataTable/index.vue
+++ src/components/ADempiere/DataTable/index.vue
@@ -147,7 +147,7 @@ export default {
     },
     getHeigthTable() {
       if (this.getDataDetail !== 'undefined' && this.panelType !== 'window') {
-        return this.$store.getters.getHeigth() - 220
+        return this.$store.getters.getHeigth() - 240
       } else {
         return this.$store.getters.getHeigth() - 400
       }
