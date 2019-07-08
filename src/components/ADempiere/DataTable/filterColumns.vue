0653225f9f3d11540693514e0c835cf34d00b548
Raul Muñoz <rmunoz@erpcya.com>
2019-07-08 10:53:44 -0400

    Update filterColumns.vue

--- src/components/ADempiere/DataTable/filterColumns.vue
+++ src/components/ADempiere/DataTable/filterColumns.vue
@@ -8,7 +8,7 @@
       size="mini"
       collapse-tags
       value-key="key"
-      class="select-fil"
+      class="select-filter"
       @change="addField"
     >
       <el-option
@@ -86,7 +86,7 @@ export default {
 </script>
 <style>
 
-  .select-fil{
+  .select-filter{
     height: 28px;
   }
 
