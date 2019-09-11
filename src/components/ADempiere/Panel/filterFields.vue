887ffbda63e9761cf64801bd0c2d86b7a9cb1840
elsiosanchez <45974454+elsiosanchez@users.noreply.github.com>
2019-09-11 11:51:33 -0400

    support FilterFields (#470)

--- src/components/ADempiere/Panel/filterFields.vue
+++ src/components/ADempiere/Panel/filterFields.vue
@@ -77,3 +77,20 @@ export default {
   }
 }
 </script>
+<style>
+  .el-tag--small {
+    height: 24px;
+    padding: 0 8px;
+    line-height: 22px;
+    max-width: 65%;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+  .el-select .el-tag__close.el-icon-close {
+    background-color: #C0C4CC;
+    right: 0px;
+    top: 0;
+    color: #FFFFFF;
+  }
+</style>
