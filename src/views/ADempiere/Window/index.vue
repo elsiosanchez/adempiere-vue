97c8be2332b4d2b191de4118b64d62000613c03d
Esanchez <elsiosanches@gmail.com>
2019-09-16 09:58:01 -0400

    Support scroll split panel

--- src/views/ADempiere/Window/index.vue
+++ src/views/ADempiere/Window/index.vue
@@ -26,7 +26,7 @@
           <template slot="paneR">
             <el-container style="height: 88vh;">
               <Split direction="vertical" @onDrag="onDrag">
-                <SplitArea :size="isShowedTabChildren ? 50 : 100">
+                <SplitArea :size="isShowedTabChildren ? 50 : 100" :style="isShowedTabChildren ? { overflow: 'auto'} : { overflow: 'hidden'} ">
                   <el-header style="height: 39px;">
                     <context-menu
                       :menu-parent-uuid="$route.meta.parentUuid"
@@ -37,7 +37,7 @@
                       :modal-metadata="windowMetadata"
                     />
                   </el-header>
-                  <el-main>
+                  <el-main :style="isShowedTabChildren ? { height: 'initial', overflow: 'auto'} : { height: 'initial', overflow: 'hidden'} ">
                     <tab-parent
                       :window-uuid="windowUuid"
                       :tabs-list="windowMetadata.tabsListParent"
