3c1b043bb00fd3bca8feb1c1950e76abfb07bde1
Leonel Matos <matosleonel0@gmail.com>
2019-09-19 21:23:42 -0400

    refactor recent items component template (#482)
    * refactor recent items component template
    
    * changes in search method

--- src/components/ADempiere/RecentItems/index.vue
+++ src/components/ADempiere/RecentItems/index.vue
@@ -1,34 +1,23 @@
 <template>
   <div class="recent-items">
-    <!-- <div class="header">
-      <el-input v-model="search" :placeholder="$t('table.recentItems.search')" />
-    </div>
     <el-table
-      v-loading="isLoaded"
-      :data="recentItems.filter(data => !search || data.displayName.toLowerCase().includes(search.toLowerCase()) || data.description.toLowerCase().includes(search.toLowerCase()) || String(data.updated).toLowerCase().includes(search.toLowerCase()))"
-      fit
-      highlight-current-row
-      style="width: 100%;"
+      :data="(search.length > 0) ? filterResult(search) : recentItems"
       @row-click="handleClick"
     >
-      <el-table-column :label="$t('table.recentItems.date')">
-        <template slot-scope="{row}">
-          <span>{{ row.updated | parseTime('{d}-{m}-{y} {h}:{i}') }}</span>
+      <el-table-column
+        label=""
+        prop="displayName"
+      >
+        <template slot="header" slot-scope="scope">
+          <el-input
+            v-model="search"
+            size="mini"
+            :placeholder="$t('table.dataTable.search')"
+            :minlength="scope.$index"
+          />
         </template>
       </el-table-column>
-      <el-table-column :label="$t('table.recentItems.name')" prop="displayName" sortable />
-      <el-table-column :label="$t('table.recentItems.description')" prop="description" sortable />
-    </el-table> -->
-    <el-card
-      v-for="(item, index) in recentItems"
-      :key="index"
-      shadow="never"
-      class="card-box"
-      @click.native="handleClick(item)"
-    >
-      <span class="card-content">{{ item.displayName }}</span><br>
-      <time class="time">{{ item.updated }}</time>
-    </el-card>
+    </el-table>
   </div>
 </template>
 
@@ -41,7 +30,8 @@ export default {
     return {
       recentItems: [],
       isLoaded: true,
-      search: ''
+      search: '',
+      accentRegexp: /[\u0300-\u036f]/g
     }
   },
   computed: {
@@ -77,15 +67,21 @@ export default {
       }
     },
     handleClick(row) {
-      this.$router.push({ name: row.menuUuid, query: { action: (row.uuidRecord) ? row.uuidRecord : 'create-new' }})
+      this.$router.push({ name: row.menuUuid, query: { action: (row.uuidRecord) ? row.uuidRecord : 'create-new', tabNumber: 0 }})
     },
     subscribeChanges() {
       this.$store.subscribe((mutation, state) => {
-        // The mutation comes in the format of `{ type, payload }`.
         if (mutation.type === 'setRecentItems') {
           this.recentItems = this.getterRecentItems
         }
       })
+    },
+    filterResult(search) {
+      return this.recentItems.filter(item => this.ignoreAccent(item.displayName).toLowerCase().includes(this.ignoreAccent(search.toLowerCase())))
+    },
+    ignoreAccent(s) {
+      if (!s) { return '' }
+      return s.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
     }
   }
 }
