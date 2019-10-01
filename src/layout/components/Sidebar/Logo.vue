fc90d9958d7380dab2f38f85f2d1a4bcde1d3a52
Esanchez <elsiosanches@gmail.com>
2019-10-01 12:02:13 -0400

    tooltip position right

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -2,7 +2,7 @@
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link sidebar-logo-link-close" to="/">
-        <el-tooltip placement="top">
+        <el-tooltip placement="right">
           <div slot="content">{{ getRol.name }} | {{ getRol.clientName }}</div>
           <img v-if="logo" :src="logo" class="sidebar-logo">
           <h1 v-else class="sidebar-title">{{ title }} </h1>
@@ -11,7 +11,7 @@
       <div v-else key="expand" class="sidebar-logo-link">
         <img v-if="logo" :src="logo" class="sidebar-logo" @click="dashboard()">
         <h1 class="sidebar-title" @click="dashboard()">{{ title }}</h1><br>
-        <el-tooltip placement="top">
+        <el-tooltip placement="right">
           <div slot="content">{{ getRol.name }} | {{ getRol.clientName }}</div>
           <p class="sidebar-sub-title" @click="profile()">
             {{ getRol.name }} | {{ getRol.clientName }}
