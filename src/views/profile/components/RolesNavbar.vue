974c41d28b19f2f8ce1fd93c0d144bceb31dccb9
Esanchez <elsiosanches@gmail.com>
2019-10-02 14:45:49 -0400

    Structure Profile

---
+++ src/views/profile/components/RolesNavbar.vue
@@ -0,0 +1,117 @@
+<template>
+  <el-form>
+    <el-select
+      v-model="value"
+      :filterable="!isMobile"
+      value-key="key"
+      @change="handleChange"
+    >
+      <el-option
+        v-for="(item, key) in getRoles"
+        :key="key"
+        :label="item.name"
+        :value="item.uuid"
+      />
+    </el-select>
+  </el-form>
+</template>
+
+<script>
+import { getLanguage } from '@/lang'
+import { showMessage } from '@/utils/ADempiere'
+import { resetRouter } from '@/router'
+
+export default {
+  name: 'RolesNavar',
+  data() {
+    return {
+      value: '',
+      options: [],
+      languageList: [],
+      language: ''
+    }
+  },
+  computed: {
+    getRol() {
+      return this.$store.getters['user/getRol']
+    },
+    getRoles() {
+      return this.$store.getters['user/getRoles']
+    },
+    languageCookie() {
+      return getLanguage()
+    },
+    getterLanguageList() {
+      return this.$store.getters.getLanguageList(this.getRol.uuid)
+    },
+    isMobile() {
+      return this.$store.state.app.device === 'mobile'
+    },
+    permissionRoutes() {
+      return this.$store.getters.permission_routes
+    }
+  },
+  created() {
+    this.value = this.getRol.uuid
+    this.getLanguageData()
+  },
+  methods: {
+    showMessage,
+    resetRouter,
+    handleChange(valueSelected) {
+      this.$message({
+        message: this.$t('notifications.loading'),
+        iconClass: 'el-icon-loading'
+      })
+      this.$store.dispatch('user/changeRoles', valueSelected)
+        .then(response => {
+          this.showMessage({
+            message: this.$t('notifications.successChangeRole'),
+            type: 'success'
+          })
+          this.$store.dispatch('permission/generateRoutes', response.name)
+            .then(response => {
+              this.resetRouter()
+              response.forEach((element) => {
+                this.$router.resolve(element)
+              })
+              this.$router.addRoutes(response)
+            })
+          this.$store.dispatch('getRecentItemsFromServer')
+        })
+    },
+    changeLanguage(languageValue) {
+      this.language = languageValue
+    },
+    getLanguageList(open) {
+      if (open) {
+        if (this.getterLanguageList.length) {
+          this.languageList = this.getterLanguageList
+        } else {
+          this.getLanguageData()
+        }
+      }
+    },
+    getLanguageData() {
+      var tableLanguage = 'AD_Language'
+      this.$store.dispatch('getObjectListFromCriteria', {
+        containerUuid: this.getRol.uuid,
+        tableName: tableLanguage,
+        query: `SELECT * FROM ${tableLanguage}`,
+        whereClause: `LanguageIso = '${this.languageCookie}' AND IsActive = 'Y'`
+      })
+        .then(response => {
+          this.languageList = response.map(language => {
+            return {
+              value: language.AD_Language,
+              label: language.Name
+            }
+          })
+        })
+        .catch(error => {
+          console.warn('Error getting language list:', error.message + '. Code: ', error.code)
+        })
+    }
+  }
+}
+</script>
