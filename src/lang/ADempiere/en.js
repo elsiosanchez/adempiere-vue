4946ae9f08a0e0665884a54689775da2c75f9b13
Raul Muñoz <rmunoz@erpcya.com>
2019-10-14 16:35:40 -0400

    Update en.js

--- src/lang/ADempiere/en.js
+++ src/lang/ADempiere/en.js
@@ -130,7 +130,7 @@ export default {
   table: {
     ProcessActivity: {
       name: 'Name',
-      zoonIn: 'Zoon in',
+      zoomIn: 'Zoom in',
       description: 'Description',
       actions: 'Action',
       status: 'Status',
