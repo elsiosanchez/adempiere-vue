b53640338ad1ebd50a74c2fe96cf555eaedf13bb
EdwinBetanc0urt <EdwinBetanc0urt@hotmail.com>
2019-10-29 17:23:56 -0400

    fix: Add new record data tables. (#726)

--- src/views/ADempiere/Window/index.vue
+++ src/views/ADempiere/Window/index.vue
@@ -57,7 +57,7 @@
           <template slot="paneR">
             <el-container style="height: 86vh;">
               <Split direction="vertical" @onDrag="onDrag">
-                <SplitArea :size="isShowedTabChildren ? 50 : 100" :style="isShowedTabChildren ? { overflow: 'auto'} : { overflow: 'hidden'}">
+                <SplitArea :size="isShowedTabChildren ? 50 : 100" :style="isShowedTabChildren ? { overflow: 'auto' } : { overflow: 'hidden' }">
                   <el-header style="height: 39px;">
                     <context-menu
                       v-show="!isShowedRecordPanel"
@@ -69,7 +69,7 @@
                       :modal-metadata="windowMetadata"
                     />
                   </el-header>
-                  <el-main :style="isShowedTabChildren ? { height: 'initial', overflow: 'auto'} : { height: 'initial', overflow: 'hidden'}">
+                  <el-main :style="isShowedTabChildren ? { height: 'initial', overflow: 'auto' } : { height: 'initial', overflow: 'hidden' }">
                     <tab-parent
                       :window-uuid="windowUuid"
                       :tabs-list="windowMetadata.tabsListParent"
